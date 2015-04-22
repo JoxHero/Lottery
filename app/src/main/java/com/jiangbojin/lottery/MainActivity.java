50351332408e2453392632c3e930b759a71b59ee
riverking <jinjiangbo0605@163.com>
2015-04-22 11:05:26 +0800

    增加左边的滑动菜单

--- app/src/main/java/com/jiangbojin/lottery/MainActivity.java
+++ app/src/main/java/com/jiangbojin/lottery/MainActivity.java
@@ -4,16 +4,55 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends ActionBarActivity {
+   private MyClickListener myClickListener;
+   private int[] menu_items = {R.id.My_history};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        InitSlidingMenu();
+
     }
 
+    protected void InitSlidingMenu() {
+        // configure the SlidingMenu
+        SlidingMenu menu = new SlidingMenu(this);
+        menu.setMode(SlidingMenu.LEFT);
+        menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+        menu.setShadowWidthRes(R.dimen.shadow_width);
+        menu.setShadowDrawable(R.drawable.shadow);
+        menu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+        menu.setFadeDegree(0.35f);
+        menu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
+        menu.setMenu(R.layout.slidingmenu);
+        InitSlidingMenuItem();
+    }
+
+    protected void InitSlidingMenuItem() {
+        myClickListener = new MyClickListener();
+        for (int i =0; i<menu_items.length; i++)
+        {
+            LinearLayout menu_history = (LinearLayout)findViewById(menu_items[i]);
+            menu_history.setOnClickListener(myClickListener);
+        }
+
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -36,4 +75,16 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+    public class MyClickListener implements View.OnClickListener
+    {
+
+        @Override
+        public void onClick(View v) {
+            if (v.getId() == R.id.My_history)
+            {
+
+            }
+        }
+    }
+
 }
